6c7fba32df4566812a3cf5e57d811a4078509a8b
ZillaCoop <cooprix1@gmail.com>
2023-09-20 15:48:40 +0200

    Reads customer by name

--- ChinookDb/DataAccess/ChinookDAO.cs
+++ ChinookDb/DataAccess/ChinookDAO.cs
@@ -49,7 +49,7 @@ namespace ChinookDb.DataAccess
             return customers;
         }
 
-        public List<Customer> GetCustomerById(int CustomerId)
+        public List<Customer> ReadCustomerById(int CustomerId)
         {
             List<Customer> customers = new List<Customer>();
             string sql = "SELECT CustomerId, FirstName, LastName, PostalCode, Country, Phone, Email " +
@@ -58,6 +58,30 @@ namespace ChinookDb.DataAccess
             conn.Open();
             using SqlCommand cmd = new SqlCommand(sql, conn);
             cmd.Parameters.AddWithValue("@CustomerId", CustomerId);
+            var reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                customers.Add(new Customer(
+                    reader.GetInt32(0),
+                    reader.GetString(1),
+                    reader.GetString(2),
+                    reader.IsDBNull(3) ? null : reader.GetString(3),
+                    reader.IsDBNull(4) ? null : reader.GetString(4),
+                    reader.IsDBNull(5) ? null : reader.GetString(5),
+                    reader.GetString(6)));
+            }
+            return customers;
+        }
+
+        public List<Customer> ReadCustomersByName(string name)
+        {
+            List<Customer> customers = new List<Customer>();
+            string sql = "SELECT CustomerId, FirstName, LastName, PostalCode, Country, Phone, Email " +
+                "FROM Customer WHERE FirstName LIKE @Name OR LastName LIKE @Name";
+            using SqlConnection conn = new SqlConnection(GetConnectionString());
+            conn.Open();
+            using SqlCommand cmd = new SqlCommand(sql, conn);
+            cmd.Parameters.AddWithValue("@Name", "%" + name + "%");
             var reader = cmd.ExecuteReader();
             while (reader.Read())
             {
--- ChinookDb/Program.cs
+++ ChinookDb/Program.cs
@@ -8,7 +8,9 @@ namespace ChinookDb
         {
             ChinookDAO chinookDAO = new ChinookDAO();
             //chinookDAO.ReadAllCustomers().ForEach(c => Console.WriteLine(c));
-            chinookDAO.GetCustomerById(1).ForEach(c => Console.WriteLine(c));
+            //chinookDAO.ReadCustomerById(1).ForEach(c => Console.WriteLine(c));
+            chinookDAO.ReadCustomersByName("Jon").ForEach(c => Console.WriteLine(c));
+
         }
     }
 }
