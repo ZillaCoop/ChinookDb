628e33a6a4195b0931e0400f8c559ab4d5cd4cdc
ZillaCoop <cooprix1@gmail.com>
2023-09-20 14:51:14 +0200

    Added interfaces

---
+++ ChinookDb/Repositories/Customers/ICustomerRepository.cs
@@ -0,0 +1,14 @@
+﻿using ChinookDb.DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChinookDb.Repositories.Customers
+{
+    internal interface ICustomerRepository : ICrudRepository<Customer, int>
+    {
+
+    }
+}
---
+++ ChinookDb/Repositories/ICrudRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChinookDb.Repositories
+{
+    internal interface ICrudRepository<T, TKey>
+    {
+        List<T> GetAll();
+        List<T> GetById(TKey id);
+
+        bool Add(T obj);
+        bool Update(T obj);
+        bool Delete(T obj);
+        bool DeleteById(T obj);
+    }
+}
