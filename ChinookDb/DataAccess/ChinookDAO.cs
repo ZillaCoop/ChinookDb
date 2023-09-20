8575be5bcc1cb5e70d654c972f32e79b8a69c286
ZillaCoop <cooprix1@gmail.com>
2023-09-20 09:56:21 +0200

    Add project files.

---
+++ ChinookDb/DataAccess/ChinookDAO.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChinookDb.DataAccess
+{
+    internal class ChinookDAO
+    {
+        public ChinookDAO()
+        {
+            using SqlConnection conn = new SqlConnection(GetConnectionString());
+            conn.Open();
+        }
+
+        private static string GetConnectionString()
+        {
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.DataSource = "N-NO-01-01-4419\\SQLEXPRESS";
+            builder.InitialCatalog = "Chinook";
+            builder.IntegratedSecurity = true;
+            builder.TrustServerCertificate = true;
+            return builder.ConnectionString;
+        }
+
+
+    }
+}
---
+++ ChinookDb/Program.cs
@@ -0,0 +1,12 @@
+﻿using ChinookDb.DataAccess;
+
+namespace ChinookDb
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ChinookDAO chinookDAO = new ChinookDAO();
+        }
+    }
+}
